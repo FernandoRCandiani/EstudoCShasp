aae868ea152baae06cffefef762b1a4b130fdd8a
Fernando Candiani <candianirfernando@gmail.com>
2023-12-14 10:02:54 -0300

    Novo projeto e ajuste nas pastas dos projetos

---
+++ SegundoProjeto/SegundoProjeto/Entities/Post.cs
@@ -0,0 +1,30 @@
+﻿namespace SegundoProjeto.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+
+        public List<Post> Posts { get; set; } = new List<Post>();
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post() { }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddPost(Post post) { Posts.Add(post); }
+        public void RemovePost(Post post) { Posts.Remove(post); }
+        public void AddComment(Comment comment) { Comments.Add(comment); }
+        public void RemoveComment(Comment comment) { Comments.Remove(comment); }
+
+    }
+}
---
+++ SegundoProjeto/SegundoProjeto/Program.cs
@@ -0,0 +1,54 @@
+﻿using SegundoProjeto.Entities;
+
+namespace SegundoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qtde de post: ");
+            int nPost = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nPost; i++)
+            {
+                Console.Write("Titulo do post: ");
+                string title = Console.ReadLine();
+                DateTime date = DateTime.Now;
+                Console.Write("Explique sua conteudo: ");
+                string content = Console.ReadLine();
+                Console.Write("Contidade de like: ");
+                int likes = int.Parse(Console.ReadLine());
+
+               Post post = new Post(date, title, content, likes);
+                post.AddPost(post);
+
+                Console.WriteLine("-----------------");
+                Console.Write("Qtde de comentario: ");
+                int nComment = int.Parse(Console.ReadLine());
+
+                for (int  j = 0; j < nComment; j++)
+                {
+                    Console.Write("texto do comentario: ");
+                    string text = Console.ReadLine();
+
+                    Comment comment = new Comment(text);
+                    post.AddComment(comment);
+                }
+
+                Console.WriteLine("-----------------");
+                Console.WriteLine(post.Title);
+                Console.WriteLine(post.Likes + " Likes, " + post.Moment);
+                Console.WriteLine(post.Content);
+                Console.WriteLine("Comments:");
+
+                for (int x = 0; x < post.Comments.Count; x++)
+                {
+                    Console.WriteLine(post.Comments[x].Text);
+                }
+            Console.WriteLine("-----------------");
+            }
+
+
+        }
+    }
+}
