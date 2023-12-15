d5f30ff701679ff6a0e14a24564d01568c9e69ca
Fernando Candiani <candianirfernando@gmail.com>
2023-12-15 15:19:20 -0300

    Projeto - Heranca e porlimorfismo

---
+++ Funcionario/Funcionario/Program.cs
@@ -0,0 +1,47 @@
+﻿using Funcionario.Entities;
+using System.Globalization;
+
+namespace Funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Qtde de funcionarios: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Funcionario #{i + 1} data: ");
+                Console.Write("Terceirizado (y / n)? ");
+                char ch = char.Parse(Console.ReadLine().ToUpper());
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Horas: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value por Hora: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if(ch == 'Y')
+                {
+                    Console.Write("Cobraça adicional: ");
+                    double additionalCharge = double.Parse(Console.ReadLine() , CultureInfo.InvariantCulture);
+
+                    list.Add(new OutsourceEmployee(name, hours, valuePerHour, additionalCharge));
+                } else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pagamentos:");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
